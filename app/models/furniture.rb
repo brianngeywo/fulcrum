af87816d7fa319c24a0e71d5ad4b2c82810a32e4
brianngeywo <brianngeywo254@gmail.com>
2020-10-01 12:00:34 +0300

    some stuff

--- app/models/furniture.rb
+++ app/models/furniture.rb
@@ -3,7 +3,7 @@ class Furniture < ApplicationRecord
     belongs_to :user
     scope :latest, -> {order created_at: :desc} 
     
-    scope :for_display, -> { where(for_sale: false, status: "for display") } 
+    scope :for_display, -> { where(for_sale: false) } 
     scope :for_sale, -> {where(for_sale: true, status: "available") } 
     scope :sold, -> { where(for_sale: true, status: "sold") } 
     scope :available, -> { where status: "available" }
--- config/environment.rb
+++ config/environment.rb
@@ -5,10 +5,10 @@ require_relative 'application'
 Rails.application.initialize!
 ActionMailer::Base.delivery_method = :smtp
  ActionMailer::Base.smtp_settings = {
-  :user_name => 'apikey',
-  :password => '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',
-  :domain => 'yourdomain.com',
-  :address => 'smtp.sendgrid.net',
+  :user_name => "#{Rails.application.credentials.twilio[:user_name]}",
+  :password => "#{Rails.application.credentials.twilio[:password]}"
+  :domain => "#{Rails.application.credentials.twilio[:domain]}",
+  :address => "#{Rails.application.credentials.twilio[:address]}",
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
